8ac66c21cd229c7ebf2b9e8772462808366cc420
ChristopherGoodale <christophergdale@gmail.com>
2023-01-02 23:22:55 -0600

    Changes made to packages in index.js

--- potential-enigma/Develop/index.js
+++ potential-enigma/Develop/index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: "input",
+    message: "What is your email?",
+    name: "github"},];   
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt( questions)
+    .then(response => {
+        writeToFile("README_TODO1.md", response)
+    })
+}
 
 // Function call to initialize app
 init();
